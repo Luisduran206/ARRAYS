5e1a0599cdc55bb5f6e0a6d5ae1d728c2e42fcae
Luisduran206 <112398598+Luisduran206@users.noreply.github.com>
2023-02-09 20:59:45 -0600

    Add files via upload

---
+++ arreglos.java
@@ -0,0 +1,13 @@
+package parcialdos;
+
+public class arreglos {
+
+	public static void main(String[] args) {
+		
+		int[] myArray = new int[15];
+		myArray[9] = 5;
+		
+		String[] nombres = new String[20];
+		nombres[0] = "Francisco";
+	}
+}
---
+++ student.java
@@ -0,0 +1,90 @@
+package parcialdos;
+import java.util.Arrays;
+public class student {
+
+	String name;
+	int ID;
+	int age;
+	int x;
+	
+	String[] objects = new String[100];
+	
+
+	//Métodos del arreglo de los enteros
+	public String buscarEntero(int[]myArray, int x) {
+		String newString = "";
+		for(int i = 0; i < myArray.length; i++) {
+			if(myArray[i] == x) {
+				newString = "El número que busca está en la posición " + x;
+				break;
+			}else {
+				continue;
+			}
+	    }
+		return newString;
+	}
+
+	public String mostrarEnteros(int[]myArray) {
+		String end = "";
+		for(int x = 0; x < myArray.length; x++) {
+			end += myArray[x];
+		}
+		return end;
+	}
+	
+	public void moverEntero(int[]myArray, int enteroEliminado) {
+		int[] newArray = new int[myArray.length-1];
+		for (int i = 0; i < myArray.length-1; i++) {
+            if(myArray[i] == enteroEliminado){
+                newArray = new int[myArray.length - 1];
+                for(int posicion = 0; posicion < i; posicion++){
+                    newArray[posicion] = newArray[posicion];
+                }
+                for(int x = i; x < myArray.length - 1; x++){
+                    newArray[x] = myArray[x++];
+                }
+                break;
+            }
+		}
+		System.out.println("Su nuevo arreglo de enteros es: " + Arrays.toString(newArray));
+	}
+
+	//Métodos del arreglo de las cadenas
+	public String buscarString(String[]myArray, String name) {
+		String newString = "";
+		for(int i = 0; i < myArray.length; i++) {
+			if(myArray[i].equals(name)) {
+				newString = "El nombre que busca está en la posición " + i;
+				break;
+			} else {
+				continue;
+			}
+		}
+		return newString;
+	}
+	
+	public String mostrarString(String[] myArray) {
+		String end = "";
+		for(int x = 0; x < myArray.length; x++) {
+			end += myArray[x];
+		}
+		return end;
+	}
+	
+	public void moverString(String[]myArray, String cadenaEliminada) {
+		String[] newArray = new String[myArray.length-1];
+		for(int i = 0; i < myArray.length-1; i++) {
+			if(myArray[i] == cadenaEliminada) {
+				newArray = (String[]) new String[myArray.length-1];
+				for(int posicion = 0; posicion < i; posicion++) {
+					newArray[posicion] = newArray[posicion];
+				}
+				for(int x = i; x < myArray.length-1; x++) {
+					newArray[x] = myArray[x++];
+				}
+				break;
+			}
+		}
+		System.out.println("Su nuevo arreglo de cadenas es: " + Arrays.toString(newArray));
+	}
+}
